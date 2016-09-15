5386206ae220ef8df0e638b10d62157b9854e572
Mateusz Trojak <matisq@op.pl>
2016-09-15 12:21:49 +0200

    Update run.sh

--- bamboo-agent/run.sh
+++ bamboo-agent/run.sh
@@ -2,23 +2,51 @@
 
 cd $HOME
 
+# Function used to validate if JAR File Exists
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]
+  then 
+    echo "true"
+  fi
+}
+
+# Download Bamboo Agent
 if [ -n ${AGENT_VERSION} ] && [ -n ${BAMBOO_SERVER} ] && [ -n ${BAMBOO_SERVER_PORT} ]
 then
+
   echo "Provided Data:"
   echo "AGENT_VERSION: "${AGENT_VERSION}
   echo "BAMBOO_SERVER: "${BAMBOO_SERVER}
   echo "BAMBOO_SERVER_PORT: "${BAMBOO_SERVER_PORT}
-  echo "Downloading Bamboo Agent from Bamboo Server."
-  wget http://${BAMBOO_SERVER}:${BAMBOO_SERVER_PORT}/agentServer/agentInstaller/atlassian-bamboo-agent-installer-${AGENT_VERSION}.jar
-  if [ $? == "0" ]
+  
+  AGENT_JAR="http://${BAMBOO_SERVER}:${BAMBOO_SERVER_PORT}/agentServer/agentInstaller/atlassian-bamboo-agent-installer-${AGENT_VERSION}.jar"
+  CHECK_AGENT_JAR=`validate_url $AGENT_JAR`
+  echo"AGENT_JAR: "${AGENT_JAR}
+  
+  echo "Downloading Bamboo Agent from Bamboo Server.""
+  echo "Checking if Bambo Server is setup."
+  if [ ${CHECK_AGENT_JAR}=="true" ]
   then
-    echo "Starting Bamboo Agent."
-    java -jar atlassian-bamboo-agent-installer-${AGENT_VERSION}.jar http://${BAMBOO_SERVER}:${BAMBOO_SERVER_PORT}/agentServer/
+  
+    echo "Found Bamboo Agent at ${AGENT_JAR}"
+    wget ${AGENT_JAR}
+    
+    if [ $? == "0" ]
+    then
+    
+      echo "Starting Bamboo Agent."
+      java -jar atlassian-bamboo-agent-installer-${AGENT_VERSION}.jar http://${BAMBOO_SERVER}:${BAMBOO_SERVER_PORT}/agentServer/
+    else
+      echo "Problem with downloading data from ${BAMBOO_SERVER}"
+      exit 2
+    fi
   else
-    echo "Problem with downloading data from ${BAMBOO_SERVER}"
-    exit 2
+  
+    echo "Waiting for a Bamboo Server Setup..."
+    sleep 60
   fi
 else
+
   echo "Not all needed data was provided."
   echo "AGENT_VERSION: "${AGENT_VERSION}
   echo "BAMBOO_SERVER: "${BAMBOO_SERVER}
